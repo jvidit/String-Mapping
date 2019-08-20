43ed6b7d3a746e77d1df567856c13ea78fddbbda
vidit jain <jvidit5@gmail.com>
2019-08-20 23:59:45 +0530

    Adding state dash move

---
+++ StateDashMove.cpp
@@ -0,0 +1,19 @@
+#include "StateDashMove.h"
+
+
+StateDashMove::StateDashMove(vector<string> v,int cost)
+{
+	this.v=v;
+	this.cost=cost;
+}
+
+
+StateDashMove::generateNeighbours()
+{
+	int k = v.size(),n = v[0].size();
+
+	for(int i=0;i<k;i++)
+	{
+
+	}
+}
---
+++ StateDashMove.h
@@ -0,0 +1,28 @@
+#ifndef STATEDASHMOVE_H
+#define STATEDASHMOVE_H
+
+#include <map>
+#include <utility> 
+#include <vector>
+#include <cstring>
+
+
+class StateDashMove
+{
+
+	vector<string> v;
+	int cost;
+
+
+public:
+
+	StateDashMove();
+
+	void generateNeighbours();
+
+
+
+
+};
+
+#endif
